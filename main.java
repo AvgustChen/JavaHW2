ac090213ac34faf12f9e052860c3418888d53889
AvgustChen <avchen@yandex.ru>
2022-11-27 19:48:16 +0300

    all done

--- main.java
+++ main.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class main {
     public  static String result = "";
     public static void main(String[] args) {
-        int[] array = program.createArray(30);
-        array = program.array(array, 0, 100);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        int size = in.nextInt();
+        System.out.print("Введите минимальное значение массива: ");
+        int min = in.nextInt();
+        System.out.print("Введите максимальное значение массива: ");
+        int max = in.nextInt();
+        in.close();
+        int[] array = program.createArray(size);
+        array = program.array(array, min, max);
         String printArray = program.printArray(array);
-        System.out.println(printArray);
+        System.out.printf("Изначальный массив: " + printArray + "\n");
         program.mergeSort(array);
-        System.out.println(result);
+        System.out.printf("Отсортированный массив: " + result);
     }
 }
